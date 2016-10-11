c213cedc87ec10ac31c22e8865ad41bafcd57a08
Alumno <Alumno@155.54.224.72>
2016-10-11 17:28:08 +0200

    Creación de servlet y html

---
+++ src/servlet/ServletTest.java
@@ -0,0 +1,52 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletTest
+ */
+public class ServletTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletTest() {
+        super();
+    }
+    
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("hacemos el doGet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("hacemos el doPost");
+	}
+
+
+	@Override
+	public void destroy() {
+		super.destroy();
+		System.out.println("hacemos el destroy");
+	}
+
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		System.out.println("hacemos el init");
+	}
+	
+
+}
